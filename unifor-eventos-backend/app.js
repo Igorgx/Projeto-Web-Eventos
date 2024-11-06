239d908fc4f0d15d14b013f169f7018f1054ad4d
Bowie <136846969+Kainan7@users.noreply.github.com>
2024-11-05 23:27:10 -0300

    Adicionar unifor-eventos-backend

---
+++ unifor-eventos-backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const eventRoutes = require('./routes/eventRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Rotas
+app.use('/api/events', eventRoutes);
+app.use('/api/users', userRoutes);
+
+// Porta do servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
